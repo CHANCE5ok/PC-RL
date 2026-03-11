e4083875638ed10495be4630821e85bd65c32b5b
tazievvadim07-wq <tazievvadim07@gmail.com>
2026-03-11 08:30:30 +0500

    возврат

--- paper-cups-&-routine-light/assets/minigame/button_kond.gd
+++ paper-cups-&-routine-light/assets/minigame/button_kond.gd
@@ -2,4 +2,4 @@ extends Button
 
 
 func _on_pressed() -> void:
-	get_tree().change_scene_to_file("res://assets/minigame/pastry_minigame.tscn")
+	get_tree().change_scene_to_file("res://assets/minigame/conditerskaya.tscn")
